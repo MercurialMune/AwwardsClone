1e134dcc8fc8d5c76b8a633ed5a6f6ed2a0eaad6
root <root@localhost.localdomain>
2019-03-18 10:11:34 -0400

    add a urls file to controll templating an views

--- awewards/views.py
+++ awewards/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def welcome(request):
+    current_user = request.user
+
+
+    return render(request, 'welcome.html', locals())
