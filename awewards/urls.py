92f57aaef7958df36cbba3aea9403211f39ff60d
root <root@localhost.localdomain>
2019-03-18 10:16:15 -0400

    add url paths and authentication modules

---
+++ awewards/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from awewards import views
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+
+urlpatterns=[
+    url('^$',views.welcome,name = 'welcome'),
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
